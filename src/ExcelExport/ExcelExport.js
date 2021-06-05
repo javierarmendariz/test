861ba504478cb38a84c418c675f68625a2b37ff8
jear2003@hotmail.com <javier.armendariz@zefr.com>
2021-06-05 02:52:50 -0700

    initial commit

---
+++ src/ExcelExport/ExcelExport.js
@@ -0,0 +1,148 @@
+import React, { useState } from "react";
+import GC from "@grapecity/spread-sheets";
+import { SpreadSheets, Worksheet } from "@grapecity/spread-sheets-react";
+import "@grapecity/spread-sheets-charts";
+import * as spreadExcel from "@grapecity/spread-excelio";
+import saveAs from "file-saver";
+
+const data = [
+  {
+    name: "A",
+    numberOne: 42000,
+    numberTwo: 8000
+  },
+  {
+    name: "B",
+    numberOne: 49000,
+    numberTwo: 1000
+  },
+  {
+    name: "C",
+    numberOne: 49000,
+    numberTwo: 1000
+  },
+  {
+    name: "D",
+    numberOne: 40000,
+    numberTwo: 10000
+  },
+  {
+    name: "E",
+    numberOne: 30000,
+    numberTwo: 20000
+  },
+  {
+    name: "F",
+    numberOne: 30000,
+    numberTwo: 20000
+  },
+  {
+    name: "G",
+    numberOne: 25000,
+    numberTwo: 25000
+  },
+  {
+    name: "H",
+    numberOne: 10000,
+    numberTwo: 40000
+  },
+  {
+    name: "I",
+    numberOne: 3000,
+    numberTwo: 7000
+  },
+  {
+    name: "J",
+    numberOne: 2000,
+    numberTwo: 8000
+  },
+  {
+    name: "K",
+    numberOne: 25000,
+    numberTwo: 25000
+  }
+];
+
+const ExcelExport = () => {
+  const [spread, setSpread] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const workbookInit = (spreadInstance) => {
+    setSpread(spreadInstance);
+  };
+
+  const importFile = async () => {
+    imporFiletAsync();
+    const excelFile = await imporFiletAsync();
+    const excelIO = new spreadExcel.IO();
+    excelIO.open(
+      excelFile,
+      (json) => {
+        spread.fromJSON(json);
+      },
+      (e) => {
+        console.log(e);
+      }
+    );
+  };
+
+  const imporFiletAsync = async () => {
+    const file = await fetch("/static/excel.xlsx");
+    const blobFile = await file.arrayBuffer();
+    return blobFile;
+  };
+
+  const exportFile = () => {
+    const excelIO = new spreadExcel.IO();
+    let fileName = "Excel";
+    if (fileName.substr(-5, 5) !== ".xlsx") {
+      fileName += ".xlsx";
+    }
+
+    const json = JSON.stringify(spread.toJSON({ includeBindingSource: true }));
+    excelIO.save(
+      json,
+      (blob) => {
+        saveAs(blob, fileName);
+      },
+      (e) => {
+        console.log(e);
+      }
+    );
+  };
+
+  const addData = () => {
+    const sheet = spread.sheets[3];
+    sheet.tables.addFromDataSource("table1", 0, 0, data);
+    const tableStyle = GC.Spread.Sheets.Tables.TableThemes.light1;
+    const theTable = sheet.tables.findByName("table1");
+    theTable.style(tableStyle);
+  };
+
+  const generatesFile = async () => {
+    setIsLoading(true);
+    await importFile();
+    setTimeout(() => {
+      addData();
+      exportFile();
+      setIsLoading(false);
+    }, 1000);
+  };
+
+  return (
+    <div>
+      <input
+        type="button"
+        id="loadExcel"
+        value="Export"
+        onClick={generatesFile}
+        disabled={isLoading}
+      />
+      <SpreadSheets workbookInitialized={workbookInit}>
+        <Worksheet />
+      </SpreadSheets>
+    </div>
+  );
+};
+
+export default ExcelExport;
